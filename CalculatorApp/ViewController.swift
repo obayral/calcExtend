43809d1ed8dc32ab4fab3828785522da83dab85d
OguzBayral <oguzbayral93@gmail.com>
2018-09-25 17:03:59 -0400

    Fixed the Decimal Point Bug, added some functionalities

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -18,16 +18,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     
     private var userCurrentlyTyping = false
+    private var hasDecimal = false
     
     @IBAction private func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         
         if userCurrentlyTyping{
-            let textCurrentlyInDisplay = label.text!
-            label.text = textCurrentlyInDisplay + digit
+            if hasDecimal == false{
+                let textCurrentlyInDisplay = label.text!
+                label.text = textCurrentlyInDisplay + digit
+                if digit == "."{
+                    hasDecimal = true
+                }
+            }else if hasDecimal == true && digit != "."{
+                label.text = label.text! + digit
+            }
         } else{
-            label.text = digit
-            userCurrentlyTyping = true
+            if hasDecimal == false && digit == "." {
+                label.text = label.text! + digit
+                userCurrentlyTyping = true
+                hasDecimal = true
+            }else{
+                label.text = digit
+                userCurrentlyTyping = true
+            }
         }
         
     }
@@ -48,15 +62,25 @@ class ViewController: UIViewController {
         case "AC":
             operandStack.removeAll()
             displayIntValue = 0
+            hasDecimal = false
         case "π":
             displayValue = Double.pi
+            hasDecimal = true
         case "e":
             displayValue = M_E
+            hasDecimal = true
+        case "cos":
+            performUnaryOperation(operation:{cos($0)})
+        case "sin":
+            performUnaryOperation(operation:{sin($0)})
+        case "%":
+            performUnaryOperation(operation:{($0/100)})
+        case "+/-":
+            displayValue.negate()
         default: break
         }
     
     }
-    
     
     func performBinaryOperation(operation:(Double,Double)->Double){
         if operandStack.count >= 2 {
@@ -89,7 +113,7 @@ class ViewController: UIViewController {
             userCurrentlyTyping = false
         }
     }
-    private var displayIntValue: Int{
+     var displayIntValue: Int{
         get{
             return Int(label.text!)!
         }
@@ -101,6 +125,8 @@ class ViewController: UIViewController {
     }
     
 }
+
+
 
 
 
